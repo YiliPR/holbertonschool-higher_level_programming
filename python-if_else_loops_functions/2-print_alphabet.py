2924bc8e4838c3e84c54d8db2a69ab37c6e64a90
yilipr <elijfigueroaperez@gmail.com>
2025-05-11 22:19:53 -0400

    yes

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from string import ascii_lowercase
 n = ascii_lowercase
 
--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from string import ascii_lowercase
 n = (','. join(letter for letter in ascii_lowercase if letter not in ('q', 'e')))
 
