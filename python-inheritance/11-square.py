10595a6044b6745bc8cd90d44af0e41b3c7f9f03
yilipr <elijfigueroaperez@gmail.com>
2026-08-23 09:49:32 -0400

    Finished the exercises 7 - 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+Reactangle = __import__('9-rectangle').Rectangle
+
+class Square(Reactangle):
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size,size)
+
+    def area(self):
+        return super().area()
+
+    def __str__(self):
+        return (f"[Square] {self.__size}/{self.__size}")
--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+class BaseGeometry:
+    
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        self.name = name
+        self.value = value
+
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
